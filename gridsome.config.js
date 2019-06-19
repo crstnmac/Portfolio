629bd0a069ce8e5c9265c85306a02734d5f182d7
Cris <criston2011@gmail.com>
2019-06-19 23:24:24 +0530

    github pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,7 +17,6 @@ module.exports = {
   siteName: 'Criston-Mascarenhas',
   siteDescription: 'A simple portfolio.',
   siteUrl: 'https://cristonkrizz.github.io',
-  pathPrefix: '/cristonkrizzz.github.io',
   plugins: [
     {
       use: '@gridsome/source-filesystem',
