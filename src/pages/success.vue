e7a55e881d514c7ca58cb3a5e23c8e614ba19f61
Cris <criston2011@gmail.com>
2019-06-20 11:16:08 +0530

    📦 NEW: contact form success page

---
+++ src/pages/success.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-16">Thankyou <br /> Your message has been successfully sent.</h2>
+      <g-image src="../../static/thankyou.svg" />
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Thankyou'
+  }
+}
+</script>
