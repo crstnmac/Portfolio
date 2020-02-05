ebf0314373c21f473a3fc48f6d5371bb7be446f1
criston <criston2011@gmail.com>
2020-02-05 20:07:54 +0530

    new post on vscodium

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,34 +1,30 @@
 <template>
   <Layout>
-    <div class="container-inner mx-auto py-8 ">
-      <h1 class=" text-4xl font-bold leading-tight py-4">Blog</h1>
-    
-    
+    <div class="container-inner mx-auto py-8">
+      <h1 class="text-4xl font-bold leading-tight py-4">Blog</h1>
       <div
         v-for="post in $page.posts.edges"
         :key="post.id"
-        class="post rounded-lg neumorph  mb-12 p-4"
+        class="post rounded-lg neumorph mb-12 p-4"
       >
         <g-link :to="post.node.path" class="text-3xl font-bold">
           {{
-            post.node.title
+          post.node.title
           }}
         </g-link>
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
-          <span> &middot; </span>
+          <span>&middot;</span>
           <span>{{ post.node.timeToRead }} min read</span>
         </div>
 
-        <div class="text-lg mb-4">
-          {{ post.node.summary }}
-        </div>
+        <div class="text-lg mb-4">{{ post.node.summary }}</div>
 
         <!-- <div class="mb-8">
           <g-link :to="post.node.path" class="font-bold uppercase"
             >Read More</g-link
           >
-        </div> -->
+        </div>-->
       </div>
       <!-- end post -->
 
